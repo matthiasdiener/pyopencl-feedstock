feef5315aa08e3fbd6b968eb8c5bd426354e8ce3
Matthias Diener <matthias.diener@gmail.com>
2021-02-15 17:31:20 -0600

    remove unused build.sh code

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,5 @@
 set -e
 
-platform='unknown'
-unamestr="$(uname)"
-
-if [[ "$unamestr" == 'Linux' ]]; then
-    platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-    platform='freebsd'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    platform='osx'
-fi
-
 ./configure.py --cl-inc-dir=$PREFIX/include --cl-lib-dir=$PREFIX/lib --cl-libname=OpenCL
 
 python setup.py install --single-version-externally-managed --record record.txt
